9ce70d4351e4edbf2ff39b04de18c8cb0d8f8328
prosperemebo <emeboprosper@gmail.com>
2024-04-18 16:07:26 +0100

    New Push

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """ Type-annotated function for sum_list """
 
+from typing import List
 
-def sum_list(input_list: list[int]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """Type-annotated implementation for sum_list"""
     return sum(input_list)
