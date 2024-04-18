bf79d84ddeb989cfd58184bb009c04756a3a1129
prosperemebo <emeboprosper@gmail.com>
2024-04-18 16:06:04 +0100

    Create 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Type-annotated function for sum_list """
+
+
+def sum_list(input_list: list[int]) -> float:
+    """Type-annotated implementation for sum_list"""
+    return sum(input_list)
