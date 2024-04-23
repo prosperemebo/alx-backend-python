4522073aebdb3dbfa9f1b6e8f54fdfbcd2ff8cb3
prosperemebo <emeboprosper@gmail.com>
2024-04-24 00:17:17 +0100

    New Push

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     The coroutine will loop 10 times,
     each time asynchronously wait 1 second
