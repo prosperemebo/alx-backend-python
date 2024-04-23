ebb9caa847acc8ab2b1f635db82a3266914bf92c
prosperemebo <emeboprosper@gmail.com>
2024-04-24 00:14:13 +0100

    Task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" script for async_generator """
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    The coroutine will loop 10 times,
+    each time asynchronously wait 1 second
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
