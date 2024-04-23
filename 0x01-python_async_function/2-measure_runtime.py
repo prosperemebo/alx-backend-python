e083c4572391aa95583c311a977448f7eb766090
prosperemebo <emeboprosper@gmail.com>
2024-04-23 01:29:38 +0100

    New Push

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -12,5 +12,4 @@ def measure_time(n: int, max_delay: int) -> float:
     start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     elapsed_time = time.perf_counter - start_time
-
     return elapsed_time / n
