5733a05536d4e86f3151dd909b6033671df9a506
prosperemebo <emeboprosper@gmail.com>
2024-05-13 23:24:44 +0100

    Task 0

---
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -0,0 +1,3 @@
+# 0x03. Unittests and Integration Tests
+
+0x03-Unittests_and_integration_tests
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" this module contains TestAccessNestedMap implementations """
+
+from typing import Mapping, Sequence, Any
+from parameterized import parameterized
+from utils import access_nested_map
+from unittest import TestCase
+
+class TestAccessNestedMap(TestCase):
+    """Test cases for the access_nested_map function."""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(
+            self,
+            n_map: Mapping,
+            path: Sequence,
+            expected: Any,
+        ):
+        """Test successful access_nested_map."""
+        self.assertEqual(access_nested_map(n_map, path), expected)
