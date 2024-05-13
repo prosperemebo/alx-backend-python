8c4e38f3a5ed9438b6ae9d4adddef076b983ee2b
prosperemebo <emeboprosper@gmail.com>
2024-05-13 23:32:24 +0100

    Task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,19 +6,37 @@ from parameterized import parameterized
 from utils import access_nested_map
 from unittest import TestCase
 
+
 class TestAccessNestedMap(TestCase):
     """Test cases for the access_nested_map function."""
 
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
     def test_access_nested_map(
-            self,
-            n_map: Mapping,
-            path: Sequence,
-            expected: Any,
-        ):
+        self,
+        n_map: Mapping,
+        path: Sequence,
+        expected: Any,
+    ):
         """Test successful access_nested_map."""
         self.assertEqual(access_nested_map(n_map, path), expected)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",)),
+            ({"a": 1}, ("a", "b")),
+        ]
+    )
+    def test_access_nested_map_exception(
+        self,
+        n_map: Mapping,
+        path: Sequence,
+    ):
+        """Test expected exceptions during access_nested_map"""
+        with self.assertRaises(KeyError):
+            access_nested_map(n_map, path)
